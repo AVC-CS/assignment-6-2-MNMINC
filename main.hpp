dc3c5e69d5df99939fc99746f43b7126f223c72f
MNMINC <157410433+MNMINC@users.noreply.github.com>
2024-03-13 17:56:35 +0000

    Reverse

--- main.hpp
+++ main.hpp
@@ -16,11 +16,11 @@ int getRandom(void)
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
         int retnum = (rdnum1 + rdnum2 + rdnum3) / 3;
-        if (abs(retnum - rdnum1) > abs(retnum - rdnum2) && abs(retnum - rdnum1) > abs(retnum - rdnum3))
+        if (abs(retnum - rdnum1) < abs(retnum - rdnum2) && abs(retnum - rdnum1) < abs(retnum - rdnum3))
                 return rdnum1;
-        else if (abs(retnum - rdnum2) > abs(retnum - rdnum1) && abs(retnum - rdnum1) > abs(retnum - rdnum3))
+        else if (abs(retnum - rdnum2) < abs(retnum - rdnum1) && abs(retnum - rdnum1) < abs(retnum - rdnum3))
                 return rdnum2;
-        else if (abs(retnum - rdnum3) > abs(retnum - rdnum2) && abs(retnum - rdnum1) > abs(retnum - rdnum1))
+        else if (abs(retnum - rdnum3) < abs(retnum - rdnum2) && abs(retnum - rdnum1) < abs(retnum - rdnum1))
                 return rdnum3;
         else
                 return retnum;
